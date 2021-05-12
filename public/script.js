013d83ca979b5583c04352d948a26161d6bf71e1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-12 17:48:05 +0530

    :bug: Fixed the connect new user method bug

--- public/script.js
+++ public/script.js
@@ -1,80 +1,77 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-// const myPeer = new Peer(undefined, {
-//   path: '/peerjs',
-//   host: '/',
-//   port: '3030'
-// })
-// let myVideoStream;
-// const myVideo = document.createElement('video')
-// myVideo.muted = true;
-// const peers = {}
-// navigator.mediaDevices.getUserMedia({
-//   video: true,
-//   audio: true
-// }).then(stream => {
-//   myVideoStream = stream;
-//   addVideoStream(myVideo, stream)
-//   myPeer.on('call', call => {
-//     call.answer(stream)
-//     const video = document.createElement('video')
-//     call.on('stream', userVideoStream => {
-//       addVideoStream(video, userVideoStream)
-//     })
-//   })
+const myPeer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '5000'
+})
+let myVideoStream;
+let myVideo = document.createElement('video')
+myVideo.muted = true;
+let peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
 
-//   socket.on('user-connected', userId => {
-//     connectToNewUser(userId, stream)
-//   })
-//   // input value
-//   let text = $("input");
-//   // when press enter send message
-//   $('html').keydown(function (e) {
-//     if (e.which == 13 && text.val().length !== 0) {
-//       socket.emit('message', text.val());
-//       text.val('')
-//     }
-//   });
-//   socket.on("createMessage", message => {
-//     $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-//     scrollToBottom()
-//   })
-// })
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    setTimeout(() => {
+      connectToNewUser(userId, stream)
+    },1000)
+  })
+  // input value
+  // let text = $("input");
+  // // when press enter send message
+  // $('html').keydown(function (e) {
+  //   if (e.which == 13 && text.val().length !== 0) {
+  //     socket.emit('message', text.val());
+  //     text.val('')
+  //   }
+  // });
+  // socket.on("createMessage", message => {
+  //   $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+  //   scrollToBottom()
+  // })
+})
 
 // socket.on('user-disconnected', userId => {
 //   if (peers[userId]) peers[userId].close()
 // })
 
-// myPeer.on('open', id => {
-//   socket.emit('join-room', ROOM_ID, id)
-// })
-
-// function connectToNewUser(userId, stream) {
-//   const call = myPeer.call(userId, stream)
-//   const video = document.createElement('video')
-//   call.on('stream', userVideoStream => {
-//     addVideoStream(video, userVideoStream)
-//   })
-//   call.on('close', () => {
-//     video.remove()
-//   })
-
-//   peers[userId] = call
-// }
-
-// function addVideoStream(video, stream) {
-//   video.srcObject = stream
-//   video.addEventListener('loadedmetadata', () => {
-//     video.play()
-//   })
-//   videoGrid.append(video)
-// }
-
-socket.emit('room-joined', ROOM_ID,10);
-socket.on('user-connected',(userId) => {
-  console.log('User Connected:' + userId);
+myPeer.on('open', id => {
+  socket.emit('room-joined', ROOM_ID,id);
 })
 
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
 
 const scrollToBottom = () => {
   var d = $('.main__chat_window');
