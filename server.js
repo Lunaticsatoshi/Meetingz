f3e0b84ec4be1b0d5f1c77448c60cd9a40f49c6c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-12 15:50:21 +0530

    :package: Application Interaction buttons

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express()
 const server = require('http').Server(app);
 const socket = require('socket.io')(server);
+const {v4: uuidV4} = require('uuid')
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/',(req,res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req,res) => {
+    res.render('room', {roomId: req.params.room});
+})
+
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
